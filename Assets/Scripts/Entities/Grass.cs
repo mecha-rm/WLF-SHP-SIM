0a16ff4d561882b14828d711b4c0e870e718a3f3
Roderick Montague <roderick.montague243@gmail.com>
2021-09-19 00:31:02 -0400

    Re-working how grass spreads.
    Going to implement a grid, so I'll be reworking how grass spreads.

--- Assets/Scripts/Entities/Grass.cs
+++ Assets/Scripts/Entities/Grass.cs
@@ -56,61 +56,64 @@ public class Grass : Living
     // spreads the grass.
     private void Spread()
     {
+        // TODO: rework to align with World3D Grid.
+        // match with grid location centers intead.
+
         // finds the world.
-        if (world == null)
-            world = FindObjectOfType<World3D>();
-
-        // no world exists.
-        if(world == null)
-        {
-            Debug.LogAssertion("Grass cannont grow without an established world.");
-            return;
-        }
-
-        // array that represents all 8 possible locations for new grass.
-        bool[,] spreadArr = new bool[3, 3];
-
-
-        // checks each section.
-        // row
-        // for(int i = 0; i < spreadArr.Length; i++)
+        // if (world == null)
+        //     world = FindObjectOfType<World3D>();
+        // 
+        // // no world exists.
+        // if(world == null)
         // {
-        //     // col
-        //     for(int j = 0; j < spreadArr.GetLength(0); j++)
+        //     Debug.LogAssertion("Grass cannont grow without an established world.");
+        //     return;
+        // }
+        // 
+        // // array that represents all 8 possible locations for new grass.
+        // bool[,] spreadArr = new bool[3, 3];
+        // 
+        // 
+        // // checks each section.
+        // // row
+        // // for(int i = 0; i < spreadArr.Length; i++)
+        // // {
+        // //     // col
+        // //     for(int j = 0; j < spreadArr.GetLength(0); j++)
+        // //     {
+        // //        
+        // //     }
+        // // }
+        // 
+        // // offset for grass
+        // Vector3 offset = boundsMax - boundsCenter;
+        // 
+        // // x (horizontal)
+        // for (int x = -1; x < 1; x++)
+        // {
+        //     // z (vertical)
+        //     for (int z = -1; z < 1; z++)
         //     {
-        //        
+        //         // current location.
+        //         if (x == 0 && z == 0)
+        //             continue;
+        // 
+        //         // calculates next position and farthest point.
+        //         Vector3 nextPos = transform.position + new Vector3(offset.x * x, 0.0F, offset.z * z);
+        //         Vector3 FarthestPoint = nextPos + new Vector3(offset.x * x, 0.0F, offset.z * z);
+        // 
+        //         // checks farthest point to see if it's in the world bounds.
+        //         if(world.InWorldBounds(FarthestPoint))
+        //         {
+        //             // TODO: check for collision with other grass.
+        //             Grass grass = EntityManager.GetInstance().CreateGrass();
+        // 
+        //             // puts grass in next position.
+        //             if (grass != null)
+        //                 grass.transform.position = nextPos;
+        //         }
         //     }
         // }
-
-        // offset for grass
-        Vector3 offset = boundsMax - boundsCenter;
-
-        // x (horizontal)
-        for (int x = -1; x < 1; x++)
-        {
-            // z (vertical)
-            for (int z = -1; z < 1; z++)
-            {
-                // current location.
-                if (x == 0 && z == 0)
-                    continue;
-
-                // calculates next position and farthest point.
-                Vector3 nextPos = transform.position + new Vector3(offset.x * x, 0.0F, offset.z * z);
-                Vector3 FarthestPoint = nextPos + new Vector3(offset.x * x, 0.0F, offset.z * z);
-
-                // checks farthest point to see if it's in the world bounds.
-                if(world.InWorldBounds(FarthestPoint))
-                {
-                    // TODO: check for collision with other grass.
-                    Grass grass = EntityManager.GetInstance().CreateGrass();
-
-                    // puts grass in next position.
-                    if (grass != null)
-                        grass.transform.position = nextPos;
-                }
-            }
-        }
 
     }
 
--- Assets/Scripts/World3D.cs
+++ Assets/Scripts/World3D.cs
@@ -5,58 +5,89 @@ using UnityEngine;
 // the 3D world.
 public class World3D : MonoBehaviour
 {
-    // the bounds of the world.
-    public Vector3 boundsMin = new Vector3(-50.0F, -50.0F, -50.0F);
-    public Vector3 boundsMax = new Vector3(50.0F, 50.0F, 50.0F);
+    // the grid for the 3D world. The grid is attached to the terrain.
+    // by default, all cells are 10 units apart. By extension, cubes are made 10 units apart as well.
+    // also by default, 1 Unity unit is equal to 1 metre (i.e. 100 centimetres)
+    public Grid grid;
 
-    // public Vector2Int gridSize;
-    // private bool[,] grid;
+    // world dimensions (cannot be changed after the game starts)
+    // make sure the size stays even.
+    public Vector3Int worldSize = new Vector3Int(10, 10, 10);
+
+    // grid for grass locations.
+    // this is 2D since there's only one grass block per z-cell position.
+    // true = grass placed, false = no grass.
+    private bool[,] grassGrid;
 
     // Start is called before the first frame update
     void Start()
     {
+        // the default world size.
+        int defWorldSize = 10;
+
+        // the world has no x-dimension
+        if (worldSize.x == 0)
+        {
+            Debug.LogWarning("No world X set. Giving default value");
+            worldSize.x = defWorldSize;
+        }
+            
+        // the world has no y-dimension
+        if (worldSize.y == 0)
+        {
+            Debug.LogWarning("No world Y set. Giving default value");
+            worldSize.y = defWorldSize;
+        }
+            
+        
+        // makes grass grid.
+        grassGrid = new bool[worldSize.x, worldSize.y];
+
         // gridSize.x = Mathf.Abs(gridSize.x);
         // gridSize.y = Mathf.Abs(gridSize.y);
         // grid = new bool[gridSize.x, gridSize.y];
     }
 
-    // gets the center of the world.
-    public Vector3 GetLocalBoundsCenter()
-    {
-        return (boundsMin + boundsMax) / 2.0F;
-    }
-
     // gets the minimum based on the world's location.
-    public Vector3 GetWorldBoundsMinimum()
+    public Vector3 GetWorldMinimum()
     {
-        return transform.position + boundsMin;
+        // grid exists.
+        if (grid != null)
+        {
+            return grid.CellToWorld(-worldSize / 2);
+        }
+        else // grid does not exist.
+        {
+            Debug.LogError("No grid set. Returning zero vector.");
+            return Vector3.zero;
+        }
     }
 
     // gets the maximum based on the world's location.
-    public Vector3 GetWorldBoundsMaximum()
+    public Vector3 GetWorldMaximum()
     {
-        return transform.position + boundsMax;
-    }
-
-    // gets the center based on the world's location.
-    public Vector3 GetWorldBoundsCenter()
-    {
-        return (2 * transform.position + boundsMin + boundsMax) / 2.0F;
-    }
-
-    // check to see if the position is within the world bounds.
-    public bool InWorldBounds(Vector3 pos)
-    {
-        return (
-            pos.x >= boundsMin.x && pos.x <= boundsMax.x &&
-            pos.y >= boundsMin.y && pos.y <= boundsMax.y &&
-            pos.z >= boundsMin.z && pos.z <= boundsMax.z
-            );
+        // grid exists.
+        if (grid != null)
+        {
+            return grid.CellToWorld(worldSize / 2);
+        }
+        else // grid does not exist.
+        {
+            Debug.LogError("No grid set. Returning zero vector.");
+            return Vector3.zero;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        // if(grid != null)
+        // {
+        //     Vector3 zero = new Vector3(100, 0, 0);
+        //     Vector3 cell = grid.WorldToCell(zero);
+        //     Debug.Log("(100, 0, 0) = " + cell.ToString());
+        // }
         
     }
 }
